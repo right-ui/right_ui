34a26ab1500a33f950a5a307272ba76fd36e1c70
c4710n <c4710n@users.noreply.github.com>
2022-03-13 22:40:20 +0800

    feat: add handy macro

--- lib/right_ui.ex
+++ lib/right_ui.ex
@@ -2,4 +2,37 @@ defmodule RightUI do
   @moduledoc """
   Documentation for `RightUI`.
   """
+
+  def component do
+    quote do
+      use Phoenix.Component
+
+      unquote(view_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(view_helpers())
+    end
+  end
+
+  defp view_helpers do
+    quote do
+      import RightUI.Helper
+    end
+  end
+
+  @doc false
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+
+  defmacro __using__(_) do
+    quote do
+      use RightUI.Layout
+    end
+  end
 end
