bd1a78d4b1847e4d1f4305738c256ee528425be4
c4710n <c4710n@users.noreply.github.com>
2022-03-21 00:17:46 +0800

    feat(layout/container): add attr - `:reverse`

--- lib/right_ui/layout/container.ex
+++ lib/right_ui/layout/container.ex
@@ -7,15 +7,16 @@ defmodule RightUI.Layout.Container do
       |> attr(:class, :string)
       # add padding from which breakpoint
       |> attr(:padding, :enum, values: [:always, :sm], default: :sm)
+      |> attr(:reverse, :boolean, default: false)
       |> attr(:breakpoint, :boolean, default: false)
-      |> attr(:extra, :rest, exclude: [:class, :padding, :breakpoint])
+      |> attr(:extra, :rest, exclude: [:class, :padding, :reverse, :breakpoint])
 
     ~H"""
     <div
       class={merge_class(container_class(@breakpoint), @class)}
       {@extra}
     >
-      <div class={padding_class(@padding)}>
+      <div class={padding_class(@padding, @reverse)}>
         <%= render_slot(@inner_block) %>
       </div>
     </div>
@@ -25,6 +26,8 @@ defmodule RightUI.Layout.Container do
   def container_class(true), do: "container mx-auto"
   def container_class(false), do: "mx-auto"
 
-  def padding_class(:always), do: "px-4 sm:px-6 lg:px-8"
-  def padding_class(:sm), do: "sm:px-6 lg:px-8"
+  def padding_class(:always, false), do: "px-4 sm:px-6 lg:px-8"
+  def padding_class(:always, true), do: "-mx-4 sm:-mx-6 lg:-mx-8"
+  def padding_class(:sm, false), do: "sm:px-6 lg:px-8"
+  def padding_class(:sm, true), do: "sm:-mx-6 lg:-mx-8"
 end
